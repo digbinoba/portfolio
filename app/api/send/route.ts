90ca49967625bbb1dc753078cda6e8a94aa1c938
Delano Igbinoba <delanoigbinoba@gmail.com>
2024-03-19 17:28:48 -0400

    fixed email routes

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,22 +1,55 @@
 // import { EmailTemplate } from '../../components/EmailTemplate';
 import { EmailTemplate } from '@/components/email-template';
 import { Resend } from 'resend';
+import { NextRequest, NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 const toEmail = process.env.MY_EMAIL;
-export async function POST(req: { json: () => PromiseLike<{ email: any; subject: any; message: any; }> | { email: any; subject: any; message: any; }; }, res: any) {
+export async function POST(req: NextRequest) {
   const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
+  const subjectLineBuilder = `${email} - ${subject}`;
+
+  console.log('server is attempting to send email');
+
   try {
     const data = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
-      to: [toEmail as string],
-      subject: email + " - " + subject,
+      to: [toEmail!],
+      subject: subjectLineBuilder,
       text: message,
     });
-    return Response.json(data);
+    console.log('server sent the email');
+    return NextResponse.json(data);
   } catch (error) {
-    return Response.json({ error });
+    console.log('sorry the server had a problem');
+    return NextResponse.json({ error });
   }
 }
+/*
+
+import { Resend } from 'resend';
+import { NextRequest, NextResponse } from 'next/server';
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: NextRequest) {
+  const { email, subject, message } = await req.json();
+  const subjectLineBuilder = `${email} - ${subject}`;
+
+  console.log('server is attempting to send email');
+
+  try {
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delanoigbinoba@gmail.com'],
+      subject: subjectLineBuilder,
+      text: message,
+    });
+    console.log('server sent the email');
+    return NextResponse.json(data);
+  } catch (error) {
+    console.log('sorry the server had a problem');
+    return NextResponse.json({ error });
+  }
+}
+*/
