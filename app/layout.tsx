0033a7a78e4156e242bc9093057fef85cd6ad506
Delano Igbinoba <delanoigbinoba@gmail.com>
2024-03-19 22:31:03 -0400

    frontend changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,14 @@ import '../globals.css';
 import StarsCanvas from '@/components/StarBackground';
 import NavBar3 from '@/components/Navbar';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from 'react';
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react';
 
 export const metadata: Metadata = {
   title: 'Delano Igbinoba - Portfolio',
   description: 'My personal portfolio website',
 };
 
-export default function RootLayout(props: { children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; }) {
+export default function RootLayout(props: { children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) {
   return (
     <html lang="en">
       <body className="bg-[#030014] overflow-y-scroll overflow-x-hidden`">
