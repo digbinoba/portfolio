e7649872df63fa67bbbadccf1f2ca5493df88d23
Delano Igbinoba <delanoigbinoba@gmail.com>
2024-03-19 22:00:53 -0400

    downgrade next

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   basePath:'/portfolio',
+  distDir: 'dir',
   images: {
     remotePatterns: [
       {
